b5e56e36a9a47dfa2cb88aa4ecc2d22300f5e22f
champakram <kiwiblokechamp@gmail.com>
2024-05-14 00:01:44 +1200

    commit3

--- main.tf
+++ main.tf
@@ -34,13 +34,13 @@ provider "aws" {
 
 # Add .gitignore file in this directory with the terraform.tfvars
 
-resource "aws_instance" "tc_instance" {
-  ami           = "ami-0c7c4e3c6b4941f0f"
-  instance_type = "t2.micro"
+# resource "aws_instance" "tc_instance" {
+#   ami           = "ami-0c7c4e3c6b4941f0f"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "TC-triggered-instance1"
-  }
-}
+#   tags = {
+#     Name = "TC-triggered-instance1"
+#   }
+# }
 
 #
